2b7115883cc03db0f17d2f269975f17eab7b937a
apokh2 <apokharel@cmu.edu>
2020-07-30 20:45:31 -0400

    Q17 R Project Commit

---
+++ et_message.R
@@ -0,0 +1,12 @@
+Dear Extraterrestrial Being,
+
+I hope your understanding of communication is nuanced enough to find the English language at least
+nominally comprehensible! In that case, I have a few questions: 
+  
+  1. What is your populace's average lifespan?
+  2. How do you communicate in your planet?
+  3. What would you say is your civilization's most imppresive technological development or innovation?
+
+Thank you for your attention!
+  
+- A human from earth!
