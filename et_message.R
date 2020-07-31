c8ecf3fead86e7b7e8d3057ccd95567bcf89412e
apokh2 <apokharel@cmu.edu>
2020-07-30 21:08:11 -0400

    R Project Message cat-formatted

--- et_message.R
+++ et_message.R
@@ -1,12 +1,11 @@
-cat("Dear Extraterrestrial Being,")
+cat("Dear Extraterrestrial Being,\n\n")
 
-cat("I hope your understanding of communication is nuanced enough to find the English language at least
-nominally comprehensible! In that case, I have a few questions:\n") 
+cat("I hope your understanding of communication is nuanced enough to find the English language at least nominally comprehensible! In that case, I have a few questions:\n") 
    
-cat("\t1. What is your populace's average lifespan?")
-cat("\t2. How do you communicate in your planet?")
-cat("\t3. What would you say is your civilization's most imppresive technological development or innovation?\n")
+cat("\t1. What is your populace's average lifespan?\n")
+cat("\t2. How do you communicate in your planet?\n")
+cat("\t3. What would you say is your civilization's most imppresive technological development or innovation?\n\n")
  
-cat("\nThank you for your attention!\n")
+cat("Thank you for your attention!\n\n")
    
 cat("- A human from Earth")
