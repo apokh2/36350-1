e1539883d32d5481adc204c347d40697b370c352
apokh2 <apokharel@cmu.edu>
2020-07-30 21:06:05 -0400

    Cat-formatted

--- et_message.R
+++ et_message.R
@@ -1,12 +1,12 @@
-# Dear Extraterrestrial Being
-# 
-# I hope your understanding of communication is nuanced enough to find the English language at least
-# nominally comprehensible! In that case, I have a few questions: 
-#   
-#   1. What is your populace's average lifespan?
-#   2. How do you communicate in your planet?
-#   3. What would you say is your civilization's most imppresive technological development or innovation?
-# 
-# Thank you for your attention
-#   
-# - A human from earth
+cat("Dear Extraterrestrial Being,")
+
+cat("I hope your understanding of communication is nuanced enough to find the English language at least
+nominally comprehensible! In that case, I have a few questions:\n") 
+   
+cat("\t1. What is your populace's average lifespan?")
+cat("\t2. How do you communicate in your planet?")
+cat("\t3. What would you say is your civilization's most imppresive technological development or innovation?\n")
+ 
+cat("\nThank you for your attention!\n")
+   
+cat("- A human from Earth")
